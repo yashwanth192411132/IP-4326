86fe68392e8860bff59bc43cf7c1f1ca81ba63b8
yashwanth192411132 <192411132.simats@saveetha.com>
2026-01-29 09:13:29 +0530

    Delete Activity 10(xmltophp).php

--- Activity 10(xmltophp).php
+++
@@ -1,9 +0,0 @@
-<?php
-  $a=simplexml_load_file("activity.xml");
-foreach($a->students as $b)
-    {
-        echo "Regno:".$b->regno."<br>";
-        echo "Name:".$b->name."<br>";
-        echo "Dept:".$b->dep."<br>";
-    }
-    ?>
